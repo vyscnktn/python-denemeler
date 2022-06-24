0c996371a202bc90b544d7c59330f97cc319062a
Veysi Can Keten <49782611+vyscnktn@users.noreply.github.com>
2022-06-24 13:46:09 +0300

    Add files via upload

---
+++ random_operations.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def random(start):
+    a = 211
+    b = 1663
+    c = 7875
+    start = (start*a*b) % c
+    return float(start)/float(c)
+
+
+liste = [random(i) for i in range(100)]
+
+
+sns.scatterplot(data=liste);
+sns.lineplot(data=liste);
+plt.show(block=True)
+
+random(1000000000000)
